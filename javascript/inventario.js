32361f10b3529966d30d74983196779a355ee3be
samu1959 <samuel.palaciosp@autonoma.edu.co>
2026-06-02 17:37:24 -0500

    get_user_information : Get user information from the HTML

---
+++ javascript/inventario.js
@@ -0,0 +1,8 @@
+let n_producto = document.getElementById("n_producto").value;
+let c_producto = document.getElementById("c_producto").value;
+let p_unit = document.getElementById("p_unit").value;
+let cantidad_producto = document.getElementById("cantidad_producto").value;
+let unidad_medida = document.getElementById("unidad_medida").value;
+let categoria_producto = document.getElementById("categoria_producto").value;
+
+
---
+++ javascript/pedidos.js
@@ -0,0 +1,8 @@
+let nom_pedidos = document.getElementById("nom_pedidos").value;
+let direc_pedidos = document.getElementById("direc_pedidos").value;
+let telefono = document.getElementById("n_celular").value;
+let email_pedidos = document.getElementById("email_pedidos").value;
+let metodo_pago = document.getElementById("metodo_pago").value;
+let pedido = document.getElementById("pedido").value;
+let referencias = document.getElementById("referencias").value;
+
